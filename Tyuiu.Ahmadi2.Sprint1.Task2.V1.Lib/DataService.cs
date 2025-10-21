ea8a2544037b90b08f8e22fe855c92df54554e3d
Ahmadi1 <Ahmadi@DS10BK3>
2025-10-20 22:46:10 -0700

    Task2

--- Tyuiu.Ahmadi2.Sprint1.Task2.V1.Lib/DataService.cs
+++ Tyuiu.Ahmadi2.Sprint1.Task2.V1.Lib/DataService.cs
@@ -3,18 +3,17 @@ using tyuiu.cources.programming.interfaces.Sprint1;
 
 namespace Tyuiu.Ahmadi2.Sprint1.Task2.V1.Lib
 {
-    public class DataService :ISprint1Task1V1
+    public class DataService : ISprint1Task2V1
     {
         private double km;
 
-        public double Calculate(double a, double x, double y)
+        
+
+        public double ConvertKmToM(int value)
         {
             return km / 1.609;
         }
 
-        public double ConvertKmToMiles(int km)
-        {
-            return km / 1.609;
-        }
+        
     }
 }
