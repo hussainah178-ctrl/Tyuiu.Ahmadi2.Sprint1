facea75e79e67b6aa7c830fac02fbbc6ecb652d2
Ahmadi1 <Ahmadi@DS10BK3>
2025-10-27 09:13:40 -0700

    Task5

--- Tyuiu.Ahmadi2.Sprint1,Task5.V2.Lib/DataService.cs
+++ Tyuiu.Ahmadi2.Sprint1,Task5.V2.Lib/DataService.cs
@@ -1,19 +1,21 @@
 ﻿using System;
 using tyuiu.cources.programming.interfaces.Sprint1;
 
-namespace Tyuiu.Ahmadi2.Sprint1.Task5.V2
+namespace Tyuiu.Ahmadi2.Sprint1.Task5.V2.Lib;
+
+public class DataService : ISprint1Task5V2
 {
-    public class DataService :ISprint1Task5V2
+    public int FahrenheitToCelsius(double fahrenheit)
     {
-        private int fahrenheit;
+        // Formula: (F - 32) * 5/9
+        double celsius = (fahrenheit - 32) * 5 / 9;
 
-        public int FahrenheitToСelsius(double temp)
-        {
-            // Formula: (F - 32) * 5/9
-            double celsius = (fahrenheit - 32) * 5 / 9;
+        // Convert to integer using Convert class
+        return Convert.ToInt32(celsius);
+    }
 
-            // Convert to integer using Convert class
-            return Convert.ToInt32(celsius);
-        }
+    public int FahrenheitToСelsius(double temp)
+    {
+        throw new NotImplementedException();
     }
 }
